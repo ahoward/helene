5bcfbed7c0c0c5b6c05b1b2787126976ed269e69
James Edward Gray II <james@graysoftinc.com>
2009-06-13 15:07:07 -0500

    For now, I've just pathed the test loader to read AWS details from ~/.aws.yml as qval does.  I don't see any issues with this in deployment since it only applies to the tests, but we can revise the logic down the road, if needed.

--- test/auth.rb
+++ test/auth.rb
@@ -3,6 +3,29 @@
 # ENV['AWS_ACCESS_KEY_ID']
 # ENV['AWS_SECRET_ACCESS_KEY']
 #
-  Helene.aws_access_key_id     # raises error unless configured
-  Helene.aws_secret_access_key # raises error unless configured
-  Helene.aws_ca_file               # does not raise - just bootstrap
+
+config = YAML.load((File.read(File.expand_path("~/.aws.yml")) rescue "{}"))
+
+if access_key_id = config["access_key_id"]
+  ENV['ACCESS_KEY_ID']            = access_key_id
+  ENV['AMAZON_ACCESS_KEY_ID']     = access_key_id
+  ACCESS_KEY_ID                   = access_key_id
+  AMAZON_ACCESS_KEY_ID            = access_key_id
+end
+
+if secret_access_key = config["secret_access_key"]
+  ENV['SECRET_ACCESS_KEY']        = secret_access_key
+  ENV['AMAZON_SECRET_ACCESS_KEY'] = secret_access_key
+  ACCESS_SECRET_KEY               = secret_access_key
+  AMAZON_SECRET_ACCESS_KEY        = secret_access_key
+end
+
+if ca_file = config["ca_file"]
+  ENV['CA_FILE']        = ca_file
+  ENV['AMAZON_CA_FILE'] = ca_file
+  CA_FILE               = ca_file
+  AMAZON_CA_FILE        = ca_file
+  
+  # It's too late to count on helene.rb to load this, so we need to do it:
+  Rightscale::HttpConnection.params[:ca_file] = ca_file
+end
